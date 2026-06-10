ebf6376c1537243b647b24dd321d4c2b478130de
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-06-10 07:25:19 -0700

    chore: restore repository hygiene files

--- .tmp-tests/src/components/rider/section-card.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SectionCard = SectionCard;
-const utils_1 = require("@/lib/utils");
-function SectionCard({ children, className }) {
-    return (<section className={(0, utils_1.cn)('rounded-[2rem] border border-slate-200/80 bg-white px-5 py-4 shadow-lg', className)}>
-      {children}
-    </section>);
-}
